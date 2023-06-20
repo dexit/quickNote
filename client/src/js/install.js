ffb34866e97d241cca22c1e3d049be3fffe436c4
dExIT <dexit@dyc.lv>
2023-06-20 21:19:05 +0100

    updated install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
+// Logic for installing the PWA
+// Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-        window.deferredPrompt = event;
+   
+     window.deferredPrompt = event;
+
      butInstall.classList.toggle('hidden', false);
 });
 
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+     return;
+    
+    promptEvent.prompt();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+ 
+    window.deferredPrompt = null;
+});
