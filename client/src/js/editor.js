bda779a7a03268b1e82f7e63f7a0afed8b072c6d
dExIT <dexit@dyc.lv>
2023-06-20 21:57:57 +0100

    cleanup

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,12 +1,9 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
 export default class {
   constructor() {
     const localData = localStorage.getItem('content');
-
-    // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
@@ -22,12 +19,10 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
-    //  this.editor.setValue(data[data.length - 1].content || localData || header);
+  
     });
 
     this.editor.on('change', () => {
