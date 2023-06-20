d02af5264df41a346a64f12b96ef8a5804cd819f
dExIT <dexit@dyc.lv>
2023-06-20 21:22:04 +0100

    setting up the header ad editor

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,11 +26,13 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+   //   this.editor.setValue(data || localData || header);
+      this.editor.setValue(data[data.length - 1].content || localData || header);
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
+      
     });
 
     // Save the content of the editor when the editor itself is loses focus
